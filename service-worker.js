5ae431abad673025dbb225bc7039c694289bda66
pezzaliapp <pezzalialessandro@gmail.com>
2025-02-09 15:29:10 +0100

    Aggiunto supporto PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "xcompare-cache-v2"; // Cambia la versione per forzare aggiornamento
+const CACHE_NAME = "xcompare-cache-v2";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -11,38 +11,35 @@ const urlsToCache = [
 
 // Install Service Worker
 self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Installing...");
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("[Service Worker] Caching files...");
       return cache.addAll(urlsToCache);
     })
   );
-});
-
-// Fetch Requests
-self.addEventListener("fetch", (event) => {
-  console.log("[Service Worker] Fetching:", event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  self.skipWaiting();
 });
 
 // Activate Service Worker
 self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activating new Service Worker...");
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log("[Service Worker] Deleting old cache:", cache);
             return caches.delete(cache);
           }
         })
       );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
