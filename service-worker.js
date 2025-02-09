33d9890a1cc100f10eb31521be325d2a884a0a36
pezzaliapp <pezzalialessandro@gmail.com>
2025-02-09 13:13:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,43 @@
 const CACHE_NAME = "xcompare-cache-v1";
-const FILES_TO_CACHE = [
-  "index.html",
-  "styles.css",
-  "app.js",
-  "manifest.json",
-  "XTM-192.png",
-  "XTM-512.png"
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/manifest.json",
+  "/XTM-192.png",
+  "/XTM-512.png"
 ];
 
+// Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
+// Fetch Requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
